15c4e9549b73c779dfb26688b82498808f589e87
al002 <al002@disroot.org>
2023-10-24 00:32:24 +0800

    parse path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 // Uncomment this block to pass the first stage
-use std::io::{Write, Read};
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(mut stream: TcpStream) {
+    println!("accepted new connection");
+    let buf: &mut [u8; 128] = &mut [0; 128];
+    stream.read(buf);
+    let request_str = String::from_utf8(buf.to_vec()).unwrap();
+
+    let lines: Vec<&str> = request_str.split("\r\n").collect();
+    let first = lines[0];
+    let first_parts: Vec<&str> = first.split_whitespace().collect();
+    let path = first_parts[1];
+
+    if path == "/" { 
+        stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+        stream.write_all(b"HTTP/1.1 404 NOT FOUND\r\n\r\n");
+    }
+
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,12 +31,7 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let buf: &mut [u8; 128] = &mut [0; 128];
-                stream.read(buf);
-                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
-            }
+            Ok(mut _stream) => handle_connection(_stream),
             Err(e) => {
                 println!("error: {}", e);
             }
