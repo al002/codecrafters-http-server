687b7cf99a15c6bac22d4205a4b84c1e740870ac
al002 <al002@disroot.org>
2023-10-24 23:54:21 +0800

    tokio concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 // Uncomment this block to pass the first stage
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, Error};
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
     println!("accepted new connection");
     let buf: &mut [u8; 128] = &mut [0; 128];
-    stream.read(buf);
+    stream.read(buf).await?;
     let request_str = String::from_utf8(buf.to_vec()).unwrap();
 
     let lines: Vec<&str> = request_str.split("\r\n").collect();
@@ -15,12 +15,12 @@ fn handle_connection(mut stream: TcpStream) {
 
     match path {
         "/" => {
-            stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
-        },
-        "/user-agent" =>  {
+            stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").await?;
+        }
+        "/user-agent" => {
             let mut ua = "";
             for l in lines {
-                if l.contains("User-Agent") {
+                if l.contains("User-Agent: ") {
                     ua = &l[12..];
                     break;
                 }
@@ -28,9 +28,9 @@ fn handle_connection(mut stream: TcpStream) {
 
             let len = ua.len();
             let res = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", len, ua);
-            println!("{}",  res);
-            stream.write_all(res.as_bytes());
-        },
+            println!("{}", res);
+            stream.write_all(res.as_bytes()).await?;
+        }
         p if p.contains("/echo") => {
             let s = &p[6..];
             let len = s.len();
@@ -38,28 +38,29 @@ fn handle_connection(mut stream: TcpStream) {
             "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",
             len, s
         );
-            stream.write_all(res.as_bytes());
-        },
+            stream.write_all(res.as_bytes()).await?;
+        }
         _ => {
-            stream.write_all(b"HTTP/1.1 404 NOT FOUND\r\n\r\n");
+            stream.write_all(b"HTTP/1.1 404 NOT FOUND\r\n\r\n").await?;
         }
     }
+
+    Ok(())
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
+    let listener = TcpListener::bind("127.0.0.1:4221").await.unwrap();
 
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => handle_connection(_stream),
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        tokio::spawn(async move {
+            handle_connection(socket).await;
+        });
     }
 }
