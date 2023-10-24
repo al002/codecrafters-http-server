70edcdd6b04e320801da87e7cf0fb0b381cce682
al002 <al002@disroot.org>
2023-10-25 00:51:35 +0800

    read files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 // Uncomment this block to pass the first stage
+use anyhow::Result;
+use std::env;
+use std::fs;
+use std::io::{Read};
+use std::path::{PathBuf, Path};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{AsyncReadExt, AsyncWriteExt, Error};
 
-async fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
+async fn handle_connection(mut stream: TcpStream, file_path: Option<&Path>) -> Result<()> {
     println!("accepted new connection");
-    let buf: &mut [u8; 128] = &mut [0; 128];
+    let buf: &mut [u8; 2048] = &mut [0; 2048];
     stream.read(buf).await?;
     let request_str = String::from_utf8(buf.to_vec()).unwrap();
 
@@ -13,9 +18,9 @@ async fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
     let first_parts: Vec<&str> = first.split_whitespace().collect();
     let path = first_parts[1];
 
-    match path {
+    let response = match path {
         "/" => {
-            stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").await?;
+            "HTTP/1.1 200 OK\r\n\r\n".to_string()
         }
         "/user-agent" => {
             let mut ua = "";
@@ -27,40 +32,71 @@ async fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
             }
 
             let len = ua.len();
-            let res = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", len, ua);
-            println!("{}", res);
-            stream.write_all(res.as_bytes()).await?;
+            format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", len, ua)
         }
-        p if p.contains("/echo") => {
+        p if p.starts_with("/echo") => {
             let s = &p[6..];
             let len = s.len();
-            let res = format!(
-            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",
-            len, s
-        );
-            stream.write_all(res.as_bytes()).await?;
+            format!(
+                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",
+                len, s
+            )
+        }
+        p if p.starts_with("/files") => {
+            let s = &p[7..];
+            let mut p = file_path.unwrap().to_path_buf();
+            p.push(s);
+
+            let res;
+
+            match fs::read_to_string(p) {
+                Ok(data) => {
+                    res = format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",
+                        data.len(),
+                        data,
+                    );
+                }
+                _ => {
+                    res = "HTTP/1.1 404 NOT FOUND\r\n\r\n".to_string();
+                }
+            }
+
+            res
         }
         _ => {
-            stream.write_all(b"HTTP/1.1 404 NOT FOUND\r\n\r\n").await?;
+            "HTTP/1.1 404 NOT FOUND\r\n\r\n".to_string()
         }
-    }
+    };
 
+    stream.write_all(response.as_bytes()).await?;
     Ok(())
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
+    let mut directory: Option<PathBuf> = Option::None;
+
+    let mut args = env::args();
+    args.next();
+
+    if let Some("--directory") = args.next().as_deref() {
+        directory = Some(args.next().unwrap().into());
+    }
+
     let listener = TcpListener::bind("127.0.0.1:4221").await.unwrap();
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let dest = directory.clone();
 
         tokio::spawn(async move {
-            handle_connection(socket).await;
+            if let Err(e) = handle_connection(socket, dest.as_deref()).await {
+                eprintln!("{e:?}");
+            }
         });
     }
 }
